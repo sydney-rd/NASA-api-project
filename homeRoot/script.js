de623350e2bae692fe23168ae4db32a2ba8c2a76
sydney-rd <sydneydavid56@gmail.com>
2023-04-12 10:50:41 -0400

    adding JS event listeners for title/directories

--- homeRoot/script.js
+++ homeRoot/script.js
@@ -1,5 +1,18 @@
-const h1 = document.getElementById("h1");
+const h1 = document.querySelector("h1");
+const directories = document.querySelectorAll(".directories");
 
+console.log(h1)
+console.log(directories)
 h1.addEventListener("mouseover", function showHiddenContent() {
-    
+    for (let i = 0; i < directories.length; i++) {
+        directories[i].style.display = "block";
+    }
 })
+
+for (let i = 0; i < directories.length; i++) {
+    directories[i].addEventListener("mouseleave", function hideContent() {
+      for (let j = 0; j < directories.length; j++) {
+        directories[j].style.display = "none";
+      }
+    });
+  }
